0b97f42b87533445e6927e44700eeda04e38239e
Miguel <santosmiguel25@gmail.com>
2015-07-16 17:53:43 +0100

    First commit

---
+++ onlineqcl/WebContent/wp-content/themes/onqcl/header.php
@@ -0,0 +1,21 @@
+<html>
+	<head>
+		<title>Online QCL Compiler</title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+	</head>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+
+<body>
+	<div id='header'>
+		<div class='container'>
+			<nav class='navbar navbar-default'>
+				<div class='container-fluid'>
+					<div class='navbar-header'>
+						
+						<a class='navbar-brand' href='<?php bloginfo('template_url'); ?>'><?php bloginfo('title'); ?></a>
+					</div>
+				</div>
+			</nav>
+		</div>
+	</div>
---
+++ onlineqcl/WebContent/wp-content/themes/onqcl/index.php
@@ -0,0 +1,9 @@
+<?php get_header(); ?>
+	
+	<div class='container'>
+		<div class='well'>
+			<?php bloginfo(); ?>
+		</div>
+	</div>
+
+<?php get_footer(); ?>
